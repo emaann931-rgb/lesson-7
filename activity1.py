01d560fb03e77e7bbfd1aca34ddd30b83add1d2c
Noor13456 <noorgrps@gmail.com>
2025-09-14 18:50:20 +0500

    1

---
+++ activity1.py
@@ -0,0 +1,35 @@
+#Revision class
+print("Noor")
+
+#printing a statement using a variable
+a="Noor"
+print(a)
+
+#datatypes in python
+#String
+a="5"
+print(a)
+print(type(a))
+#integer
+b=5
+print(b)
+print(type(b))
+#Float
+c=5.0
+print(c)
+print(type(c))
+#boolean
+d=True
+print(d)
+print(type(d))
+#typecasting int to str
+a=5
+
+b=str(a)
+
+print(b)
+print(type(b))
+
+
+
+
